b92559185d06c40e3e300a0a64e03c92fc0b7154
Júlia Gomes <gomesjulia621@gmail.com>
2023-06-18 22:23:18 -0300

    feat: Adding security test

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const app = require('./index')
 const request = require('supertest')
+const assert = require('assert')
 
 describe('GET /', function () {
   it('responde com status 200', function (done) {
@@ -9,14 +10,32 @@ describe('GET /', function () {
   it('responde com a mensagem "Hello, World!"', function (done) {
     request(app).get('/').expect('Hello, World!', done)
   })
-
-  it('responde com os cabeçalhos de segurança apropriados', function (done) {
-    request(app).get('/').expect('Content-Security-Policy', done)
-  })
 })
 
 describe('GET /greeting/:name', function () {
   it('responde com status 200 e uma saudação personalizada', function (done) {
     request(app).get('/greeting/John').expect(200).expect('Hello, John!', done)
+  })
+})
+
+describe('Teste de segurança para evitar injeção de código', function () {
+  it('Não deve executar código injetado', function (done) {
+    const injectedCode = '<script>alert("Vulnerabilidade!");</script>'
+
+    request(app)
+      .get(`/?name=${encodeURIComponent(injectedCode)}`)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) {
+          done(err)
+        } else {
+          assert.strictEqual(
+            res.text.includes(injectedCode),
+            false,
+            'Código injetado não deve ser executado'
+          )
+          done()
+        }
+      })
   })
 })
